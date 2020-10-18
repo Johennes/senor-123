8312a3b2cef1140119b37f4bf21dab3b7d5b39d0
Johannes Marbach <n0-0ne@mailbox.org>
2020-10-18 20:52:02 +0200

    Updates

---
+++ precache-manifest.819888324f0c7b39b90cdbd74e339fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403aeb9476d6f1529311085a4b4c63e1",
+    "url": "/senor-123/index.html"
+  },
+  {
+    "revision": "bd25e55a8cfe7b0e7ceb",
+    "url": "/senor-123/static/css/main.2c041431.chunk.css"
+  },
+  {
+    "revision": "eb4c78f1ebcf0ceab5ab",
+    "url": "/senor-123/static/js/2.780a8cac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/senor-123/static/js/2.780a8cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd25e55a8cfe7b0e7ceb",
+    "url": "/senor-123/static/js/main.8ddfdca9.chunk.js"
+  },
+  {
+    "revision": "536b64684bf15ba7c25a",
+    "url": "/senor-123/static/js/runtime-main.72d9bfdb.js"
+  }
+]);
